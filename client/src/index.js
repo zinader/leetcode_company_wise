05ddc38cad1029fe6e5a92acc8efb30d044992e6
Pulkit Aggarwal <pulkit@student.unimelb.edu.au>
2021-06-23 15:41:01 +0530

    firebase-config

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase";
+import firebaseConfig from "./firebaseConfig";
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
