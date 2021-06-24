ab5dfba80384ba9abb86ea0d3d480439e58a27bd
Pulkit Aggarwal <pulkit@student.unimelb.edu.au>
2021-06-24 21:21:54 +0530

    coursera-bug

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,18 @@
 const Footer = () => {
-    return (
-        <div className='footer'>
-            <p>Created with <span>&hearts;</span> by <a target='_blank' rel="noreferrer" href='https://github.com/zinader'>Pulkit Agarwal</a> &amp; <a target='_blank' rel="noreferrer" href='https://github.com/parthx9'>Parth Arora</a></p>
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <p>
+        Created with <span>&hearts;</span> by{" "}
+        <a target="_blank" rel="noreferrer" href="https://github.com/zinader">
+          Pulkit Aggarwal
+        </a>{" "}
+        &amp;{" "}
+        <a target="_blank" rel="noreferrer" href="https://github.com/parthx9">
+          Parth Arora
+        </a>
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
