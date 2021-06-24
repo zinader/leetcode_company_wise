d4b39171b4d00418532918960551784cb0c6959c
Pulkit Aggarwal <pulkit@student.unimelb.edu.au>
2021-06-24 17:27:01 +0530

    api-file-changed

--- api/app.py
+++ api/app.py
@@ -1,9 +1,5 @@
-from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
 import json
 
-app = Flask(__name__)
-CORS(app, support_credentials=True)
 
 all_questions = []
 with open('data.json') as data_file:
@@ -21,38 +17,4 @@ list_companies.sort()
 with open('companies.json', 'w') as f:
     f.write(json.dumps(list_companies, ensure_ascii=False))
 
-@app.route('/companies')
-def getCompanies():
-    
-    return jsonify(
-        data=list_companies
-    )
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
-@app.route('/companies/<company>', methods=['GET'])
-def getQuestions(company):
-    
-    return jsonify(
-        data=[question for question in all_questions if question["Company_Name"] == company]
-    )
-    
-    
-@app.route('/companies/<company>/<difficulty>', methods=['GET'])
-def getQuestionsDifficulty(company,difficulty):
-    
-    all_filter = []
-    for question in all_questions:
-        if(question["Company_Name"] == company and question["Difficulty"]==difficulty):
-            all_filter.append(question)
-            
-    return jsonify(
-    data=all_filter
-    )
-   
-if __name__ == "__main__":
-    app.run()
--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Header from "./Header";
 import CompanyComponent from "./Companies";
 
